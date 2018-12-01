635682a5c9682126a9dee8a8664db6b01a394523
Cristian <00086917@uca.edu.sv>
2018-12-01 15:05:40 -0600

    funcion para crear una materia hecha

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -8,4 +8,26 @@ postController.create = function(req, res){
         uv: req.body.uv,
         descripcion: req.body.descripcion
     };
-}
+
+    if(data.materia && data.uv && data.descripcion && data.materia != '' && 
+    data.uv != '' && data.descripcion != ''){
+
+        let nuevaMateria = new postModel(data);
+
+        nuevaMateria.save(function(err, saved){
+            if(err){
+                res.status(500);
+                res.json({code:500, err});
+            }else{
+                res.json({ok:true, message:'se ha guardado la materia', saved});
+            }
+        });
+    }else{
+        res.status(400);
+        res.json({err:{code:400, message:'no se encontro o faltan datos', data}});
+    }
+};
+
+//exportamos el controller para poder ocuparlo fuera
+
+module.exports = postController;
